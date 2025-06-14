bf3f77f3e82b9245ded98c686f479106f2e1ab3d
Lwis2804 <luis.enrique.enc@gmail.com>
2025-06-14 10:23:25 -0600

    [ADD] se agrega el codigo para elegir diferente imagenes

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -16,8 +16,12 @@ class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
     
+    override func viewDidLoad() {
+        imageView.image = #imageLiteral(resourceName: "ball3")
+    }
+    
     @IBAction func askbuttonPressed(_ sender: UIButton) {
-        imageView.image = UIImage(named: "\(String(describing: ballArray.randomElement()))")
+        imageView.image = ballArray [Int.random(in: 0...4)]
     }
 }
 
